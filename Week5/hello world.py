f21032f361a1c94ec53f35d6925797f5d4cd1685
22bca159 <141253612+neelpatel4823@users.noreply.github.com>
2023-08-17 14:39:09 +0530

    Add files via upload

---
+++ Week5/hello world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+
+
+# In[7]:
+
+
+with open('c:/sqlite3/csv/hello world.txt','r',errors="ignore") as f:
+    contents = f.read()
+    print(contents)
+
+
+# In[ ]:
+
+
+
+
